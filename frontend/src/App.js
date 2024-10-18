a261d6095d0dff4094998d1a65868bcfd1f3973a
harshakolla18 <harshakolla18@gmail.com>
2024-10-18 18:50:03 -0500

    App

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,21 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import Signup from './components/Signup';
-import Login from './components/Login';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './components/Home';
 import Dashboard from './components/Dashboard';
+import Login from './components/Login';
+import Signup from './components/Signup';
 
 const App = () => (
   <Router>
     <Routes>
-      <Route path="/" element={<Navigate to="/signup" />} />  {/* Redirect to signup */}
-      <Route path="/signup" element={<Signup />} />
+      {/* Home page as the default landing page */}
+      <Route path="/" element={<Home />} />
+
+      {/* Signup and Login routes */}
       <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
+
+      {/* Protected route for Dashboard */}
       <Route path="/dashboard" element={<Dashboard />} />
     </Routes>
   </Router>
